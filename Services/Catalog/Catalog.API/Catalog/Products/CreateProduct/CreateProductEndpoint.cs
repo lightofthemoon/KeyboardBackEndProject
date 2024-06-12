b01c7277ed514f7e48a8fa7b68e199e45fe65a88
intern.ntquy1 <ntquy-batch43@sdc.edu.vn>
2024-06-12 17:39:36 +0700

    create product with cqrs (pending)

---
+++ Services/Catalog/Catalog.API/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using Catalog.API.DTOs;
+using Catalog.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Catalog.Products.CreateProduct;
+
+
+public record CreateProductRequest(CreateBrandDTO CreateProductDto);
+
+public record CreateProductResponse(Product Product);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/v1/cqrs/product", 
+            async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Ok(response.Product);
+            });
+    }
+}
---
+++ Services/Catalog/Catalog.API/Catalog/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,38 @@
+﻿
+using Catalog.API.Data;
+using Catalog.API.DTOs;
+using Catalog.API.Models;
+
+namespace Catalog.API.Catalog.Products.CreateProduct;
+
+public record CreateProductCommand(CreateProductDTO CreateProductDto) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Product Product);
+public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+{
+    private readonly ProductContext _context;
+    public CreateProductHandler(ProductContext context)
+    {
+        _context = context;
+    }
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Product(
+                request.CreateProductDto.ProductName,
+                request.CreateProductDto.Quantity,
+                request.CreateProductDto.Price,
+                request.CreateProductDto.Unit,
+                request.CreateProductDto.Description,
+                request.CreateProductDto.DisplayUrl,
+                request.CreateProductDto.CategoryId,
+                request.CreateProductDto.BrandId
+                );
+
+        await _context.Products.AddAsync(product);
+
+        await _context.SaveChangesAsync();
+
+        return new CreateProductResult(product);
+    }
+}
